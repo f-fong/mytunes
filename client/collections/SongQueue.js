1054e42eb6bf0b5bf25f05e540efe6c52fcdb642
f-fong <feliciasfong@gmail.com>
2016-09-28 19:41:57 -0700

    7:40 on a wed night

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,26 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function () {
+      if (this.at(1) === undefined) {
+        this.playFirst();
+      }
+    });
+    this.on('ended', function() {
+      this.remove(this.at(0));
+      if (this.at(0)) {
+        this.playFirst();
+      }
+    }, this);
+    this.on('dequeue', this.dequeue, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  dequeue: function(mod) {
+    this.remove(mod);
   }
 
 });
